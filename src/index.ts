9b78b13024a35a17c671b3b3fe71b7bb3e36a360
yuval59 <yuvalmaron222@gmail.com>
2023-06-19 02:16:58 +0300

    Changed exports

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-export {
-  FractionalBrownianMotion,
-  GradientNoise,
-  HashFunction,
-  NoiseMap,
-  RandomnessFunction,
-  ValueFunction,
-  ValueNoise,
-} from './noise'
+import * as noise from './noise'
 
-export {
-  Vector2,
-  cyrb128,
-  getFadeCurve,
-  getHashedL4PRNG,
-  getMulberry32,
-  jsf32,
-  lerp,
-  lerpCurve,
-  map_range,
-  sfc32,
-  xoshiro128ss,
-} from './utils'
+import * as utils from './utils'
+
+export default noise
+
+export { utils }
